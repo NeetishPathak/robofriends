e9629b162eeac114220fab24d526eaff7e849987
Neetish Pathak <nepathak@paypal.com>
2020-08-22 03:13:04 -0700

    Creating Robofriends App

---
+++ src/components/robots.js
@@ -0,0 +1,50 @@
+export const robots = [
+    {
+        id:1,
+        name:'Neetish Pathak',
+        username:'janedoe',
+        email:'jane.doe@gmail.com'
+    },
+    {
+        id:2,
+        name:'Patrick Wilson',
+        username:'pwilson',
+        email:'pwilson@ncsu.edu'
+    },
+    {
+        id:3,
+        name:'James Watson',
+        username:'jwatson',
+        email:'jwatson@columbia.edu'
+    },
+    {
+        id:4,
+        name:'Tim Lee',
+        username:'xtlee',
+        email:'xtlee@fb.com'
+    },
+    {
+        id:5,
+        name:'G Washington',
+        username:'gwashington',
+        email:'gwashington@wh.org'
+    },
+    {
+        id:6,
+        name:'Hasan Malik',
+        username:'hmalik',
+        email:'hmalik@emirates.com'
+    },
+    {
+        id:7,
+        name:'Indiana Jones',
+        username:'ijones',
+        email:'ij@movies.com'
+    },
+    {
+        id:8,
+        name:'hari Prasad',
+        username:'hprasad',
+        email:'hprasad@jjbakers.org'
+    }
+]
---
+++ src/containers/App.js
@@ -0,0 +1,55 @@
+
+import React, { Component } from 'react';
+import CardList from '../components/cardlist';
+import SearchBox from '../components/searchbox';
+import Scroll from '../components/scroll';
+import './App.css';
+
+
+class App extends Component {
+    constructor() {
+        
+        super();
+        this.state = {
+            robots:  [] ,
+            searchString: ''
+        }
+    }
+
+    onSearchChange = (event) => {
+        this.setState({searchString: event.target.value})
+    }
+
+    componentDidMount(){
+        
+        fetch('https://jsonplaceholder.typicode.com/users')
+        .then(response => {
+            return response.json();
+        }).then(users => {
+            this.setState({robots: users})
+        })
+    }
+
+    render() {
+        const {robots,searchString} = this.state;
+        const filteredRobots = robots.filter(robot =>
+        {
+            return robot.name.toLowerCase().includes(searchString.toLowerCase());
+        })
+
+        return !robots.length ?
+        <h1>Loading</h1>
+        :
+        ( 
+                <div className = 'tc' >
+                    <h1> Robofriends </h1>  
+                    <SearchBox searchChange={this.onSearchChange}/>
+                    <Scroll>
+                        <CardList robots = { filteredRobots }/>  
+                    </Scroll>
+                </div>
+        )
+    }
+}
+
+export default App;
